2df2f5d1f68f487818f2b07e94a4f19b207fa53f
gustavo03v <140985939+gustavo03v@users.noreply.github.com>
2024-03-01 18:22:51 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-function shortenLink() {
-    const originalLink = document.getElementById("original-link").value;
-    // Aqui você pode implementar a lógica de encurtamento do link (por exemplo, usando um serviço como Bitly ou TinyURL)
-    const shortenedLink = "https://seu-site.com/" + Math.random().toString(36).substring(7);
-    document.getElementById("shortened-link").textContent = shortenedLink;
-}
